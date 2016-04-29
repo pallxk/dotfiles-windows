f360ed1db4627ccb7b330ab310fad01cf4885f5e
pallxk <github@pallxk.com>
2016-04-29 16:27:33 +0800

    bin/rdpwho: No using psexec when running against localhost

--- bin/rdpwho.cmd
+++ bin/rdpwho.cmd
@@ -3,12 +3,22 @@ SETLOCAL
 
 @REM Show who is logged on via RDP.
 
-REM Check dependency: psexec
-where psexec >nul 2>nul || (
-	echo Not installed required dependency: psexec
-	echo See https://technet.microsoft.com/en-us/sysinternals/bb842062
-	echo Exiting
-	GOTO :EOF
+
+IF "%1" EQU "" (
+	REM Running against localhost
+	SET exec=
+) ELSE (
+	REM Running against remote host
+
+	REM Check dependency: psexec
+	where psexec >nul 2>nul || (
+		echo Not installed required dependency: psexec
+		echo See https://technet.microsoft.com/en-us/sysinternals/bb842062
+		echo Exiting
+		exit /b 1
+	)
+
+	SET exec=psexec
 )
 
-psexec %1 netstat | findstr :3389
+%exec% %1 netstat | findstr :3389
